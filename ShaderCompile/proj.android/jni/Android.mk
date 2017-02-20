05520528c957e5364f30680c3dcbb2d9d7bfe738
borsch <oleh.kurpiak@gmail.com>
2017-02-20 09:13:52 +0200

    init

---
+++ ShaderCompile/proj.android/jni/Android.mk
@@ -0,0 +1,37 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos/audio/include)
+
+LOCAL_MODULE := MyGame_shared
+
+LOCAL_MODULE_FILENAME := libMyGame
+
+# concat all cpp files pathes to one string
+PROJECT_FILES := hellocpp/main.cpp
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../../Classes/*.cpp)
+PROJECT_FILES := $(PROJECT_FILES:$(LOCAL_PATH)/%=%)
+
+LOCAL_SRC_FILES := $(PROJECT_FILES)
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+# _COCOS_HEADER_ANDROID_BEGIN
+# _COCOS_HEADER_ANDROID_END
+
+
+LOCAL_STATIC_LIBRARIES := cocos2dx_static
+
+# _COCOS_LIB_ANDROID_BEGIN
+# _COCOS_LIB_ANDROID_END
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,.)
+
+# _COCOS_LIB_IMPORT_ANDROID_BEGIN
+# _COCOS_LIB_IMPORT_ANDROID_END
